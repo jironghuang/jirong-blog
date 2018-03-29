fecd276b92f8e390c91c404279ce2b54c41f9a00
jirong <jironghuang88@gmail.com>
2018-03-29 23:24:01 +0800

    commit all

--- run_site.R
+++ run_site.R
@@ -2,7 +2,7 @@ library(blogdown)
 
 setwd("/home/jirong/Desktop/github/jirong-blog")
 # blogdown::new_site(theme = "gcushen/hugo-academic")
-blogdown::serve_site()
+# blogdown::serve_site()
 
 # blogdown::new_post("Test post")
 
