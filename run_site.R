7e7f1a75e3fa37a89946beec292078004ed36932
jirong <jironghuang88@gmail.com>
2018-03-31 23:44:12 +0800

    new updates

--- run_site.R
+++ run_site.R
@@ -2,8 +2,8 @@ library(blogdown)
 
 setwd("/home/jirong/Desktop/github/jirong-blog")
 # blogdown::new_site(theme = "gcushen/hugo-academic")
-blogdown::serve_site()
+# blogdown::serve_site()
 
 # blogdown::new_post("Test post")
 
-hugo_server(host="192.168.1.238", port=3337)    #Need to hard code ip address for it work across computers
+hugo_server(host="192.168.1.9", port=3337)    #Need to hard code ip address for it work across computers
