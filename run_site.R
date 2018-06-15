11ef6a132834bd5f997336839fb058992df1c16e
jirong <jironghuang88@gmail.com>
2018-06-15 16:02:15 +0800

    commit world cup analysis

--- run_site.R
+++ run_site.R
@@ -5,7 +5,7 @@ setwd("/home/jirong/Desktop/github/jirong-blog")
 # blogdown::serve_site()
 
 # blogdown::new_post("Testing embedding of Java JAR files in Websites/Intranet")
-blogdown::new_post("Journey from a non coder to a budding computer scientist")
+#blogdown::new_post("Journey from a non coder to a budding computer scientist")
 
 # hugo_server(host="192.168.1.238", port=3338)    #Need to hard code ip address for it work across computers
 
